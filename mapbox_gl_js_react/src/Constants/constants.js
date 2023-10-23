35e5002492908a72ee8f1bcff7d9a464d584f1d5
rolandhajos <hajosr7@hotmail.com>
2023-10-23 12:59:49 +0200

    handle map centering for one or zero markers

--- mapbox_gl_js_react/src/Constants/constants.js
+++ mapbox_gl_js_react/src/Constants/constants.js
@@ -1,5 +1,8 @@
 const token = process.env.REACT_APP_ACCESS_TOKEN;
 
+const LNG_VESZPREM = 17.9156;
+const LAT_VESZPREM = 47.0934;
+
 const ACCESS_TOKEN_FOR_LOCATION_URLS= `.json?&access_token=${token}`
 const ACCESS_TOKEN_FOR_LOCATION_URLS_AUTOCOMPLETE= `.json?autocomplete=true&access_token=${token}`
 
@@ -13,6 +16,8 @@ const GEOCODING_API_URL = `${MAPBOX_API_URL}geocoding/v5/mapbox.places/`
 
 export{
     token,
+    LNG_VESZPREM,
+    LAT_VESZPREM,
     DIRECTIONS_API,
     DIRECTIONS_PARAMS,
     GEOCODING_API_URL,
