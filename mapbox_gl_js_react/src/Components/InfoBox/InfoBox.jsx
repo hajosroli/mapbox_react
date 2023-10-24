95811efc4e85d524cb993ef864ab257496fcd021
rolandhajos <hajosr7@hotmail.com>
2023-10-24 21:50:51 +0200

    update CSS styles

--- mapbox_gl_js_react/src/Components/InfoBox/InfoBox.jsx
+++ mapbox_gl_js_react/src/Components/InfoBox/InfoBox.jsx
@@ -4,7 +4,11 @@ export default function InfoBox({lng, lat, zoom}){
     
     return(
         <div className="info-box">
-            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            Longitude: {lng}
+            <br />
+            Latitude: {lat}
+            <br />
+            Zoom: {zoom}
         </div>
     )
 }
